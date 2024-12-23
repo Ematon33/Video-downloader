fee64488db38f801fd6bec761f3b0f8f19fff0b7
Ematon33 <matvac@email.cz>
2024-12-23 11:09:55 +0100

    Added option for metadata

--- downloader.py
+++ downloader.py
@@ -9,6 +9,7 @@ def download_video(video_url, filepath):
         "outtmpl": f"{filepath}/%(title)s.%(ext)s",  # Output destination
         "audioquality": "0",  # Highest quality
         "windowsfilenames": True,  # Protect file names on Windows
+        "addmetadata": True,  # Add metadata to the video
     }
 
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
