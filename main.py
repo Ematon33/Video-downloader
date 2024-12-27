7100f9885ac987bfde31982bc9ffac4694e174b1
Ematon33 <matvac@email.cz>
2024-12-27 12:20:43 +0100

    Added threding so the download runs in different thread than progress hook

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 import gui
 import downloader
-import os
+import threading
 
 
 # Function to download the video and save it to the disk
 def download_video_process():
     video_url = app.url_text_box.get()
-    downloader.download_video(video_url, app.open_save_dialog())
 
+    folder_path = app.open_save_dialog()
+
+    t = threading.Thread(
+        target=downloader.download_video,
+        args=(video_url, folder_path, app.update_progress)
+    )
+    t.start()
 
 app = gui.GUI()
 
