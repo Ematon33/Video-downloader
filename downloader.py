4f9da3c1c17ea28ee8fb242e59237dc5fdc27f5a
Ematon33 <matvac@email.cz>
2024-12-27 12:19:43 +0100

    Added hook for progress bar

--- downloader.py
+++ downloader.py
@@ -1,8 +1,24 @@
 import yt_dlp
 
-
 # Download the video from the given URL from youtube
-def download_video(video_url, filepath):
+def download_video(video_url, filepath, progress_callback=None):
+
+    def progress_hook(d):
+        ''' 
+        Progress hook for the download 
+        '''
+        if d['status'] == 'downloading':
+            downloaded = d.get('downloaded_bytes', 0)
+            total = d.get('total_bytes') or d.get('total_bytes_estimate') or 1
+            fraction = downloaded / total 
+
+            if progress_callback:
+                progress_callback(fraction)
+
+        elif d['status'] == 'finished':
+            if progress_callback:
+                progress_callback(1.0, "finished")
+
     ydl_opts = {
         "format": "bestvideo+bestaudio/best",  # Best video and audio
         "merge_output_format": "mp4",  # Output format
@@ -10,6 +26,8 @@ def download_video(video_url, filepath):
         "audioquality": "0",  # Highest quality
         "windowsfilenames": True,  # Protect file names on Windows
         "addmetadata": True,  # Add metadata to the video
+
+        "progress_hooks": [progress_hook], # Progress hook
     }
 
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
