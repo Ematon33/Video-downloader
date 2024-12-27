206558a90d4f49b82e27511138da189c84504b7b
Ematon33 <matvac@email.cz>
2024-12-27 12:20:12 +0100

    Added progress bar and method for update of the progress bar

--- gui.py
+++ gui.py
@@ -17,7 +17,26 @@ class GUI(tk.CTk):
         self.url_text_box.pack()
         self.download_button.pack()
 
+        self.progress_bar = tk.CTkProgressBar(self, width=300)
+        self.progress_bar.set(0)  # 0 = 0%
+        self.progress_bar.pack(pady=10)
+
+        self.progress_label = tk.CTkLabel(self, text="")
+        self.progress_label.pack(pady=(0, 10))
+
     def open_save_dialog(self):
         """Opens dialog to save the file"""
         folder_path = filedialog.askdirectory(title="Select folder to save file")
         return folder_path
+    
+    def update_progress(self, fraction, eta=""):
+        def _update():
+            self.progress_bar.set(fraction)  
+            percent = fraction * 100
+            percent_text = f"{percent:.0f}%"
+            self.progress_label.configure(text=f"{percent_text}")
+
+        self.after(0, _update) 
+
+    def download_finished(self):
+        self.progress_label.configure(text="Download finished")
