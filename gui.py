fbade914b8689f9f965063558808214c5a95bba1
Ematon33 <matvac@email.cz>
2024-12-28 18:36:53 +0100

    Changed tk to CTk, Added ComboBox for file formats, Added switch for Audio/Video

--- gui.py
+++ gui.py
@@ -1,42 +1,51 @@
-import customtkinter as tk
+import customtkinter as CTk
 from tkinter import filedialog
 
 
 # Class to create the GUI
-class GUI(tk.CTk):
+class GUI(CTk.CTk):
     def __init__(self):
-        tk.CTk.__init__(self)
+        CTk.CTk.__init__(self)
         self.title("Youtube Downloader")
         self.geometry("300x150")
 
-        self.url_label = tk.CTkLabel(self, text="Youtube URL:")
-        self.url_text_box = tk.CTkEntry(self)
-        self.download_button = tk.CTkButton(self, text="Download")
+        self.url_label = CTk.CTkLabel(self, text="Youtube URL:")
+        self.url_text_box = CTk.CTkEntry(self)
+        self.download_button = CTk.CTkButton(self, text="Download")
+
+        # Dropdown to select file format for download, default is 'mp4'
+        self.file_formats = CTk.CTkComboBox(
+            self, values=["mp4", "webm", "mkv", "mp3"], state="readonly", 
+        )
+        self.file_formats.set("mp4")
+
+        self.av_switch = CTk.CTkSwitch(self, text="Audio/Video")
 
         self.url_label.pack()
         self.url_text_box.pack()
+        self.file_formats.pack()
         self.download_button.pack()
 
-        self.progress_bar = tk.CTkProgressBar(self, width=300)
+        self.progress_bar = CTk.CTkProgressBar(self, width=300)
         self.progress_bar.set(0)  # 0 = 0%
         self.progress_bar.pack(pady=10)
 
-        self.progress_label = tk.CTkLabel(self, text="")
+        self.progress_label = CTk.CTkLabel(self, text="")
         self.progress_label.pack(pady=(0, 10))
 
     def open_save_dialog(self):
         """Opens dialog to save the file"""
         folder_path = filedialog.askdirectory(title="Select folder to save file")
         return folder_path
-    
+
     def update_progress(self, fraction, eta=""):
         def _update():
-            self.progress_bar.set(fraction)  
+            self.progress_bar.set(fraction)
             percent = fraction * 100
             percent_text = f"{percent:.0f}%"
             self.progress_label.configure(text=f"{percent_text}")
 
-        self.after(0, _update) 
+        self.after(0, _update)
 
     def download_finished(self):
         self.progress_label.configure(text="Download finished")
