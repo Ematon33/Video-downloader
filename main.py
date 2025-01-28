601ed79c7e2b91503581422b43459ea7b19dcd8a
Ematon33 <matvac@email.cz>
2025-01-28 11:38:45 +0100

    Completely chanched logic for file_formats

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+import select
 import gui
 import fetch
 import threading
-import file_formats
+import info_fetcher
 
 
 def cancel_download_process():
@@ -14,7 +15,11 @@ def download_video_process():
     video_url = app.url_text_box.get()
     folder_path = app.open_save_dialog()
 
-    yt_options = file_formats.get_yt_options(app.av_switch.get(), app.file_formats.get())
+    selected_format = app.format_map[app.file_formats.get()]
+
+    yt_options = {
+        "format": selected_format,
+    }
 
     # If the user has provided a URL and a folder path, start the download
     if folder_path and video_url:
@@ -22,35 +27,19 @@ def download_video_process():
         t = threading.Thread(target=fetch.fetch_video, args=(video_url, folder_path, yt_options, app.update_progress))
         t.start()
 
+def list_formats(event=None):
+    formats = info_fetcher.get_formats(app.url_text_box.get())
 
-def switch_event():
-    """
-    This function is called whenever the audio/video switch changes.
-    It sets the available values in the ComboBox, chooses a default format,
-    and fetches the appropriate yt_options from the 'options' module.
-    """
-    audio_video_switch = app.av_switch.get()
+    app.format_map = {
+        f"{format['resolution']} - {format['ext']}": format['format_id']
+        for format in formats
+    }
 
-    if audio_video_switch:
-        app.file_formats.configure(values=file_formats.AUDIO_FORMATS)
-        app.file_formats.set(file_formats.AUDIO_FORMATS[0])
-
-    else:
-        app.file_formats.configure(values=file_formats.VIDEO_FORMATS)
-        app.file_formats.set(file_formats.VIDEO_FORMATS[0])
-
-    selected_format = app.file_formats.get()
-    yt_options = file_formats.get_yt_options(audio_video_switch, selected_format)
-
-    return yt_options
-
+    app.file_formats.configure(values=list(app.format_map.keys()))
 
 app = gui.GUI()
 
-# Attach switch_event to the switch
-app.av_switch.configure(command=switch_event)
-# Call it once so that yt_options is initialized right away
-yt_options = switch_event()
+app.url_text_box.bind("<KeyRelease>", list_formats)
 
 # Download button
 app.download_button.configure(command=download_video_process)
